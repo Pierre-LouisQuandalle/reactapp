444095a5efd4d0c8fb9d816162041717200d1fbc
Pierre-Louis <pierrelouis.quandalle@gmail.com>
2021-04-04 17:15:55 +0200

    First contract interaction

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
 import React, { Component } from 'react';
 import Web3 from 'web3';
+const song = require('./Nfts/Song')
+const toutDou = require('./Nfts/Doucement')
+const web3 = new Web3(Web3.givenProvider || "http://localhost:8545")
+//const network = await web3.eth.net.getNetworkType() 
 
-
+var SongForACity = undefined;
 class App extends Component {
   componentWillMount() {
+
     this.loadBlockchainData()
+    this.useSong()
   }
 
   async loadBlockchainData() {
-    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545")
-    const network = await web3.eth.net.getNetworkType()
     const chainID = await web3.eth.getChainId()
     this.setState({chainID})
     const lastBlock = await web3.eth.getBlockNumber()
@@ -25,13 +29,28 @@ class App extends Component {
       }
       return false;
     }     
-    ethEnabled()
-    console.log("network:", network,chainID,lastBlock,accounts[0])
+    ethEnabled()  
+  }
+  async useSong(){
+    SongForACity = new web3.eth.Contract(song.abi, song.address, {
+      from: this.state.account
+    })
+    const tokenName =await SongForACity.methods.name().call()
+    this.setState({tokenName})
+    const totalToken =await SongForACity.methods.tokenCounter().call()
+    this.setState({totalToken})
+
+    console.log("adress:", toutDou.address, song.address,tokenName,totalToken)
+    
   }
   constructor(props) {
     super(props)
     this.state = {
-      account:''
+      account:'',
+      chainID: '',
+      lastBlock: '',
+      tokenName:'',
+      totalToken:''
     }
     }
   
