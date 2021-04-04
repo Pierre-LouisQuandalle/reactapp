4c608c1c1faed976c61e62ca52095e9518e79b45
Pierre-Louis <pierrelouis.quandalle@gmail.com>
2021-04-04 16:20:25 +0200

    Premiere question

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import Web3 from 'web3';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  componentWillMount() {
+    this.loadBlockchainData()
+  }
+
+  async loadBlockchainData() {
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545")
+    const network = await web3.eth.net.getNetworkType()
+    const chainID = await web3.eth.getChainId()
+    this.setState({chainID})
+    const lastBlock = await web3.eth.getBlockNumber()
+    this.setState({lastBlock})
+    const accounts = await web3.eth.getAccounts()
+    this.setState({account:accounts[0]})
+    const ethEnabled = () => {
+      if (window.ethereum) {
+        window.web3 = new Web3(window.ethereum);
+        window.ethereum.enable();
+        return true;
+      }
+      return false;
+    }     
+    ethEnabled()
+    console.log("network:", network,chainID,lastBlock,accounts[0])
+  }
+  constructor(props) {
+    super(props)
+    this.state = {
+      account:''
+    }
+    }
+  
+  render() {
+     return (
+      <div className="container">
+      <h1>My app!</h1>
+      <p>Your account:{this.state.account} </p>
+      <p>Chain ID: {this.state.chainID}</p>
+      <p>Block number: {this.state.lastBlock}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
